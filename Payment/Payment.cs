6d70a25690361af7d1ba0f554e6f401f2518084d
Lekan Omotayo <lekan.omotayo@interswitchgroup.com>
2016-12-20 14:10:17 +0100

    first commit

---
+++ Payment/Payment.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interswitch;
+
+
+namespace Interswitch
+{
+    public class Payment
+    {
+
+        Interswitch interswitch;
+
+        public Payment(String clientId, String clientSecret, String environment = null)
+        {
+            interswitch = new Interswitch(clientId, clientSecret, environment);
+        }
+
+        public Dictionary<string, string> Authorize(string pan, string expDate, string cvv, string pin, string amt, string currency, string customerId, string reqRef)
+        {
+            string authData = interswitch.GetAuthData(pan, expDate, cvv, pin);
+            var paymentReq = new 
+            {
+               customerId = customerId,
+               amount = amt,
+               transactionRef = reqRef,
+               currency = currency,
+               authData = authData
+            };
+
+            return interswitch.Send("/api/v2/purchases", "POST", paymentReq);
+        }
+
+
+        public Dictionary<string, string> ValidateCard(string pan, string expDate, string cvv, string pin, string reqRef)
+        {
+            string authData = interswitch.GetAuthData(pan, expDate, cvv, pin);
+            var validateReq = new
+            {
+                transactionRef = reqRef,
+                authData = authData
+            };
+
+            return interswitch.Send("/api/v2/purchases/validations", "POST", validateReq);
+        }
+
+
+        public Dictionary<string, string> VerifyOTP(string tranId, string otp)
+        {
+            var verifyOTPReq = new
+            {
+                paymentId = tranId,
+                otp = otp
+            };
+
+            return interswitch.Send("/api/v2/purchases/otps/auths", "POST", verifyOTPReq);
+        }
+
+        public Dictionary<string, string> GetStatus(string reqRef, string amt)
+        {
+            Dictionary<string, string> httpHeader = new Dictionary<string, string>();
+            httpHeader.Add("amount", amt);
+            httpHeader.Add("transactionRef", reqRef);
+
+            return interswitch.Send("/api/v2/purchases", "GET", null, httpHeader);
+        }
+
+    }
+}
---
+++ SampleProject/Example.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interswitch;
+
+
+namespace SampleProject
+{
+    public class Example
+    {
+        static string clientId = "IKIA9614B82064D632E9B6418DF358A6A4AEA84D7218";
+        static string clientSecret = "XCTiBtLy1G9chAnyg0z3BcaFK4cVpwDg/GTw2EmjTZ8=";
+
+        static void Main(string[] args)
+        {
+            // Payment
+            bool hasRespCode = false;
+            bool hasRespMsg = false;
+            string httpRespCode = "400";
+            string httpRespMsg = "Failed";
+            Random rand = new Random();
+
+            string amt = "35000";
+            string currency = "NGN";
+            string custId = "customer@myshop.com";
+            var id = rand.Next(99999999);
+
+            var pan = "6280511000000095";
+            var expDate = "5004";
+            var cvv = "111";
+            var pin = "1111";
+            var otpPan = "5061020000000000011";
+            var otpExpDate = "1801";
+            var otpCvv = "350";
+            var otpPin = "1111";
+            
+            Payment payment = new Payment(clientId, clientSecret);
+            
+            // Payment - No OTP
+            var paymentReqRef = "ISW-SDK-PAYMENT-" + id;
+            var authorizeResp = payment.Authorize(pan, expDate, cvv, pin, amt, currency, custId, paymentReqRef);
+            hasRespCode = authorizeResp.TryGetValue("CODE", out httpRespCode);
+            hasRespMsg = authorizeResp.TryGetValue("RESPONSE", out httpRespMsg);
+            Console.WriteLine("Authorize HTTP Code: " + httpRespCode);
+            Console.WriteLine("Authorize HTTP Data: " + httpRespMsg);
+
+
+            var getStatusResp = payment.GetStatus(paymentReqRef, amt);
+            hasRespCode = getStatusResp.TryGetValue("CODE", out httpRespCode);
+            hasRespMsg = getStatusResp.TryGetValue("RESPONSE", out httpRespMsg);
+            Console.WriteLine("Get status HTTP Code: " + httpRespCode);
+            Console.WriteLine("Get status HTTP Data: " + httpRespMsg);
+
+
+            // Card Validation - No OTP
+            var validateReqRef = "ISW-SDK-VALIDATE-" + id;
+            var validateResp = payment.ValidateCard(pan, expDate, cvv, pin, validateReqRef);
+            hasRespCode = validateResp.TryGetValue("CODE", out httpRespCode);
+            hasRespMsg = validateResp.TryGetValue("RESPONSE", out httpRespMsg);
+            Console.WriteLine("Validate HTTP Code: " + httpRespCode);
+            Console.WriteLine("Validate HTTP Data: " + httpRespMsg);
+
+
+            // Payment requires OTP
+            var otpPaymentReqRef = "ISW-SDK-PAYMENT-OTP-" + id;
+            var otpAuthorizeResp = payment.Authorize(otpPan, otpExpDate, otpCvv, otpPin, amt, currency, custId, otpPaymentReqRef);
+            hasRespCode = otpAuthorizeResp.TryGetValue("CODE", out httpRespCode);
+            hasRespMsg = otpAuthorizeResp.TryGetValue("RESPONSE", out httpRespMsg);
+            Console.WriteLine("OTP Card Authorize HTTP Code: " + httpRespCode);
+            Console.WriteLine("OTP Card Authorize HTTP Data: " + httpRespMsg);
+            if (hasRespCode && hasRespMsg && (httpRespCode == "201" || httpRespCode == "202"))
+            {
+                Response response = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Response>(httpRespMsg);
+                var otp = "123456";
+                var otpResp = payment.VerifyOTP(response.paymentId, otp);
+                hasRespCode = otpResp.TryGetValue("CODE", out httpRespCode);
+                hasRespMsg = otpResp.TryGetValue("RESPONSE", out httpRespMsg);
+                Console.WriteLine("Payment OTP HTTP Code: " + httpRespCode);
+                Console.WriteLine("Payment OTP HTTP Data: " + httpRespMsg);
+            }
+
+            // Card Validation - requires OTP
+            var otpValidateReqRef = "ISW-SDK-VALIDATE-OTP-" + id;
+            var otpValidateResp = payment.ValidateCard(otpPan, otpExpDate, otpCvv, otpPin, otpValidateReqRef);
+            hasRespCode = otpValidateResp.TryGetValue("CODE", out httpRespCode);
+            hasRespMsg = otpValidateResp.TryGetValue("RESPONSE", out httpRespMsg);
+            Console.WriteLine("OTP Card Validate HTTP Code: " + httpRespCode);
+            Console.WriteLine("OTP Card Validate HTTP Data: " + httpRespMsg);
+
+
+            Console.ReadKey();
+        }
+                
+    }
+
+     public class Response
+    {
+        public string paymentId { get; set; }
+        public string transactionRef { get; set; }
+        public PaymentMethod[] paymentMethods { get; set; }
+    }
+
+     public class PaymentMethod
+     {
+         public string paymentMethodTypeCode { get; set; }
+         public string paymentMethodCode { get; set; }
+         public string cardProduct { get; set; }
+         public string panLast4Digits { get; set; }
+         public string token { get; set; }
+     }
+}
